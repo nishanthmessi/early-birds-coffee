bd0af67dc736aae0c3b680569952cc55e08418ab
Nishanth <nisanthrtts@gmail.com>
2023-10-14 20:04:26 +0530

    review section created

--- src/components/ReviewSection.jsx
+++ src/components/ReviewSection.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import ReviewImg from '../assets/img/review-img.png'
+import StarSvg from '../assets/svg/star.svg'
 
 const ReviewSection = () => {
-  return <div>ReviewSection</div>
+  return (
+    <div className='flex h-[878px] border-b border-primary'>
+      <div className='flex flex-col justify-center items-center px-[100px] w-[960px]'>
+        <span className='flex gap-[3px] mb-6'>
+          <img src={StarSvg} alt='' className='' />
+          <img src={StarSvg} alt='' className='' />
+          <img src={StarSvg} alt='' className='' />
+          <img src={StarSvg} alt='' className='' />
+          <img src={StarSvg} alt='' className='' />
+        </span>
+        <p className='text-[35px] uppercase mb-[53px]'>
+          "Another reason people just can't keep away from their local coffee
+          shop is the quality of coffee that's on offer."
+        </p>
+        <p className='text-[18px] space-mono'>Henry Monro</p>
+      </div>
+
+      <img src={ReviewImg} alt='' className='h-[878px] w-[960px]' />
+    </div>
+  )
 }
 
 export default ReviewSection
