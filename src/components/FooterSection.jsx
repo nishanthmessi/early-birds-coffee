6d3fdbf65335954e58346c32504ade31f2e33e9c
Nishanth <nisanthrtts@gmail.com>
2023-10-16 22:32:46 +0530

    footer created

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import FooterLogo from '../assets/img/footer-logo.png'
 
 const FooterSection = () => {
-  return <div>FooterSection</div>
+  return (
+    <div className='flex gap-[506px] items-center h-[250px] px-[70px] border-t border-primary'>
+      <div className='flex gap-10'>
+        <img
+          src={FooterLogo}
+          alt='footer-logo'
+          className='h-[102px] w-[102px]'
+        />
+
+        <h1 className='text-[30px]'>
+          Early birds
+          <span>
+            <p className='flex flex-col text-[16px] space-mono'>
+              <span>Weteringstraat 48,1017 SP</span>
+              <span>Amsterdam</span>
+              <span>Tel: 020-7718364</span>
+            </p>
+          </span>
+        </h1>
+      </div>
+      <div className='flex gap-[340px] space-mono'>
+        <ul>
+          <li>My Account</li>
+          <li>Checkout</li>
+          <li>Cart</li>
+          <li>Shop</li>
+        </ul>
+        <ul>
+          <li>Facebook</li>
+          <li>Instagram</li>
+          <li>Twitter</li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default FooterSection
